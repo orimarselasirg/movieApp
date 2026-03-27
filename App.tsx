3dfb7d22f5dcbd9f3b4ff5919dc3b332fd4a36b7
Ramiro <ramirogrisales@gmail.com>
2026-03-27 09:44:14 -0500

    Add boot splash screen support by integrating react-native-bootsplash; update App component to hide splash on load, modify AndroidManifest for theme, and add necessary drawable resources for various screen densities.

--- App.tsx
+++ App.tsx
@@ -5,13 +5,22 @@
  * @format
  */
 
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StatusBar, useColorScheme } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { AppNavigator } from '@/navigation';
+import RNBootSplash from 'react-native-bootsplash';
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
+
+  useEffect(() => {
+    const init = async () => {
+      await RNBootSplash.hide({ fade: true, duration: 2000 });
+    };
+
+    init();
+  }, []);
 
   return (
     <SafeAreaProvider style={{ marginTop: 30, flex: 1 }}>
